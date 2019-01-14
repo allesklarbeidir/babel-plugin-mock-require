19b14b4f339f4b0f81109d7a572836bb85184a59
Sebastian Jerratsch <sebastian.jerratsch@gmail.com>
2019-01-14 20:01:41 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+"use strict";
+const path = require("path");
+
+exports = module.exports = function({ types: t }) {
+    return {
+        visitor: {
+            CallExpression:{
+                exit(path, state){
+                    const args = path.node.arguments || [];
+                    if(
+                        path.node.callee.name === "require"
+                    ){
+                        path.replaceWith(
+                            t.callExpression(
+                                t.memberExpression(
+                                    t.callExpression(
+                                        t.identifier("require"),
+                                        [t.stringLiteral("babel-plugin-mock-require")]
+                                    ),
+                                    t.identifier("requireHook")
+                                ),
+                                [
+                                    t.stringLiteral(JSON.stringify(state.opts)),
+                                    t.identifier("__dirname"),
+                                    t.functionExpression(
+                                        null,
+                                        [t.identifier("require")],
+                                        t.blockStatement(
+                                            [
+                                                t.returnStatement(t.callExpression(t.identifier("require"), path.node.arguments))
+                                            ],
+                                            []
+                                        )
+                                    )
+                                ]
+                            )
+                        );
+                        path.skip();
+                    }
+                }
+            }
+        },
+    };
+}
+
+let opts = null;
+let moduleMocker = null;
+exports.requireHook = function(_opts, from, requireCallback) {
+    if(opts === null){
+        opts = JSON.parse(_opts);
+    }
+    if(!opts.moduleMocker || !typeof(opts.moduleMocker) === "string"){
+        throw new Error("Option 'moduleMocker' not specified. Please specify a module name.");
+    }
+    if(moduleMocker === null){
+        moduleMocker = require(opts.moduleMocker);
+        if(moduleMocker.__esModule && moduleMocker.default){
+            moduleMocker = moduleMocker.default;
+        }
+    }
+
+    const doRequire = function(str){
+        if(str && str.length > 0 && [".", "/"].indexOf(str[0]) !== -1){
+            return moduleMocker(path.resolve(from, str));
+        }
+        return moduleMocker(str);
+    };
+    return requireCallback(doRequire);
+};
